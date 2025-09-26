a72f1a66730748e61b62c0da47dafb175aacd73e
Decioro <decioro@yahoo.com.br>
2025-09-26 18:07:29 +0000

    Commit XadrezDecio

---
+++ XadrezDecio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    // Torre Move 5 casas para a direita
+    for (int i = 0; i < 5; i++) {
+    printf("direita:\n"); // Imprime a direção do Movimento
+    }
+
+    // Bispo Move 5 casas na diagonal cima e direita
+    int contador = 0;
+    while (contador < 5);  { 
+    printf("cima,direita:\n");
+    contador ++; 
+    }
+
+    // Rainha Move 8 casas para a esquerda
+    int i = 0; i < 8;
+    do {
+       printf("esquerda:\n"); 
+    i++;
+    } while (i = 0);
+
+    return 0;
+}   
